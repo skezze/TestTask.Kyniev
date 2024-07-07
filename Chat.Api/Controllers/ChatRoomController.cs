eb21a46daf4a13903203a85c4724bdff41a60615
Yevhen Kyniev <genya1234732@gmail.com>
2024-07-07 17:14:30 +0300

    fixed typo in controller actions routes

--- Chat.Api/Controllers/ChatRoomController.cs
+++ Chat.Api/Controllers/ChatRoomController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Chat.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class ChatRoomsController : ControllerBase
     {
         private readonly IChatRoomRepository _chatRoomRepository;
